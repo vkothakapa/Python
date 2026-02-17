89430ba2ec8e1b8e75e2fe0b14357a16b06b5e5d
NIHANYA <nihanya@NIHANYAs-MacBook-Air.local>
2026-02-17 23:35:45 +0530

    Add Fundamentals.py with basic Python syntax examples

---
+++ 01_Fundamentals/Fundamentals.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print('This is a sample Python script.')
+print('fundamentals')
+print("learning" + " " + "Python")
+
+input("what is course name? ")
+print(("learning python") + input("what is course name? "))
+
